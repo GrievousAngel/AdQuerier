1e090832f0fc052a3bfeaa982e4332065458fd63
Nick Thoresby <nick@grievousangel.co.uk>
2024-01-23 15:50:25 +0000

    Convert passwords into DevSecrets

--- Program.cs
+++ Program.cs
@@ -1,18 +1,28 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.DirectoryServices.AccountManagement;
-// using System.Linq;
+using Microsoft.Extensions.Configuration;
 
 namespace ADQuerier
 {
+    [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
     internal class Program
     {
         private const string LthtDomainName = "trust.leedsth.nhs.uk";
         private const string MyhtDomainName = "10.146.240.200:389";
+        private static string LthtAdPassword;
+        private static string MyAdPassword;
 
         private static void Main()
         {
+            var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+            LthtAdPassword = config["LthtAdPassword"];
+            MyAdPassword = config["MyAdPassword"];
+            
+            
             SearchLthtDirectory("thoresbn");
-            //SearchMythDirectory("Sarah.Zurub1");
+            //SearchMythDirectory("Amina.Rawat1");
             //SearchBySid("S-1-5-21-3363467310-3950875991-3575587589-23641");
 
             Console.Read();
@@ -36,16 +46,7 @@ namespace ADQuerier
 
         private static void SearchLthtDirectory(string accountName)
         {
-            using var ctx = new PrincipalContext(ContextType.Domain, LthtDomainName, "thoresbn", "Chatsworth36");
-
-
-            // var domain = Domain.GetDomain(new DirectoryContext(DirectoryContextType.Domain, context.Name));
-            // var domainPolicy = domain.GetDomainPolicy();
-            // return domainPolicy.MaximumPasswordAge;  // This should return a TimeSpan
-
-            
-            
-            
+            using var ctx = new PrincipalContext(ContextType.Domain, LthtDomainName, "thoresbn", LthtAdPassword);
             
             using var searcher = new PrincipalSearcher(new UserPrincipal(ctx) { SamAccountName = accountName });
             var user = searcher.FindOne();
@@ -58,14 +59,14 @@ namespace ADQuerier
             Console.WriteLine($"{user.SamAccountName} has SID = {user.Sid} : [{LthtDomainName}]");
             Console.WriteLine($"{user.DisplayName}");
             Console.WriteLine($"{user.UserPrincipalName}");
-
-            Console.WriteLine();
+            
+            // Console.WriteLine();
             // user.GetGroups().ToList().ForEach(p => Console.WriteLine(p.Name));
         }
         
         private static void SearchMythDirectory(string accountName)
         {
-            using var ctx = new PrincipalContext(ContextType.Domain, MyhtDomainName, "SVC_PPMIdentityServer_UAT@midyorks.nhs.uk", "ElSNN02t6ad");
+            using var ctx = new PrincipalContext(ContextType.Domain, MyhtDomainName, "SVC_PPMIdentityServer_UAT@midyorks.nhs.uk", MyAdPassword);
             using var searcher = new PrincipalSearcher(new UserPrincipal(ctx) { SamAccountName = accountName });
             var user = searcher.FindOne();
             if (user == null)
